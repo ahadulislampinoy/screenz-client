ef888130b7d1db3435c5e5a8fd5d01040076220a
ahadulislampinoy <ahadulislampinoy@gmail.com>
2023-01-20 20:10:54 +0600

    fix

--- pages/show/index.js
+++ pages/show/index.js
@@ -11,7 +11,7 @@ const Show = ({ shows }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div className="grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 space-y-2 my-10 px-8 mx-auto">
+      <div className="grid  grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 space-y-2 my-10 px-8 mx-auto">
         {shows.results.map((show) => (
           <ShowThumbnail key={show.id} show={show} />
         ))}
